73156739263c3df6b41e5be85f2c5eb96ca203d5
Pato Anabalón <pato.anabalon@gmail.com>
2019-05-30 17:19:06 -0400

    First commit cursos-platzi

---
+++ webpack/babel-loader/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js' 
+  },
+  module: {
+    //aquí van los loaders
+    rules: [
+      {
+        // test: que tipo de archivo quiero reconocer
+        // use: que loader se va a encargar del archivo
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader']
+        use: ExtractTextPlugin.extract({
+          // fallback: 'style-loader',
+          use: "css-loader"
+        }),
+      }
+    ]
+  },
+  plugins: [
+    //aquí van los plugin
+    new ExtractTextPlugin("css/[name].css"),
+  ]
+}
